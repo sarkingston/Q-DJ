834381a6956b6aa24ee499394bffd80b3db9106b
sidgvpta <siddharth.gvpta@gmail.com>
2016-02-01 15:02:27 +0000

    Set Temporary SplashScreen and App Logo
    Set SplashScreen and App Logos. Can be changed once a suitable logo has
    been designed.

--- app/src/main/java/ie/tcd/scss/q_dj/SplashScreen.java
+++ app/src/main/java/ie/tcd/scss/q_dj/SplashScreen.java
@@ -1,11 +1,13 @@
 package ie.tcd.scss.q_dj;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.os.Handler;
 
 /**
  * Created by UI Team on 29/01/16.
@@ -13,12 +15,34 @@ import android.view.View;
 
 public class SplashScreen extends AppCompatActivity {
 
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
