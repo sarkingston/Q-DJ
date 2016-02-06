7fe9785b7e329fbbf543f6504ff644648fdfba21
Sam Jennings <jennins@tcd.ie>
2016-02-06 21:20:08 +0000

    I'm going to delete most of what I've added here but I'm commiting it first in case I want to revert

--- app/src/main/java/ie/tcd/scss/q_dj/LoginScreen.java
+++ app/src/main/java/ie/tcd/scss/q_dj/LoginScreen.java
@@ -51,7 +51,7 @@ public class LoginScreen extends Activity implements
         btnM.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginScreen.this, HostOrClient.class));
+                startActivity(new Intent(LoginScreen.this, home.class));
             }
         });
 
