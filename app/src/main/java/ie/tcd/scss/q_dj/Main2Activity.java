888c0fc876d0885a1195f43137f0e901aed0b1a2
Sam Jennings <jennins@tcd.ie>
2016-02-19 16:29:07 +0000

    Merge branch 'UserInterface' of /Users/Sam/Documents/3rd year/Shevlin/Q-DJ with conflicts.

--- app/src/main/java/ie/tcd/scss/q_dj/Main2Activity.java
+++ app/src/main/java/ie/tcd/scss/q_dj/Main2Activity.java
@@ -10,9 +10,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
@@ -20,8 +18,10 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+
+import android.support.v7.widget.LinearLayoutManager;
+
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -30,11 +30,20 @@ import android.view.ViewGroup;
 
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -55,7 +64,7 @@ public class Main2Activity extends AppCompatActivity {
     private static final String PREF_DARK_THEME = "dark_theme";
     //private MyTask task;
     private static final String URL = "http://Alex-was-here";
-    //private URL url;
+    private URL url;
     private HttpURLConnection urlConnection;
     private List<ServerComms> customList;
     private String title, image, album;
@@ -63,16 +72,11 @@ public class Main2Activity extends AppCompatActivity {
     ArrayList<ServerComms> songList = new ArrayList<ServerComms>();
 
     private ProgressDialog progressDialog;
-    //private CardViewHelper adapter;
+    private CardViewHelper adapter;
 
     com.getbase.floatingactionbutton.FloatingActionButton skip_previous;
     com.getbase.floatingactionbutton.FloatingActionButton play;
     com.getbase.floatingactionbutton.FloatingActionButton skip_next;
-
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
-    private static String LOG_TAG = "CardViewActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,19 +86,23 @@ public class Main2Activity extends AppCompatActivity {
 
         if(useDarkTheme){
             setTheme(R.style.AppTheme2);
+            //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar2);
+            //setSupportActionBar(toolbar);
         } else{
             setTheme(R.style.AppTheme);
+            //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            //setSupportActionBar(toolbar);
+
         }
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-
-        /** Adds temp icon to action bar **/
-        //getSupportActionBar().setDisplayShowHomeEnabled(true);
-        //getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+        setContentView(R.layout.activity_main);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         tabLayout.addTab(tabLayout.newTab().setText("My Q"));
         tabLayout.addTab(tabLayout.newTab().setText("X's Q"));
+
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
         skip_previous = (com.getbase.floatingactionbutton.FloatingActionButton) findViewById(R.id.skip_previous);
@@ -115,12 +123,28 @@ public class Main2Activity extends AppCompatActivity {
         NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
         boolean connected = networkInfo != null && networkInfo.isAvailable() && networkInfo.isConnectedOrConnecting();
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-        mRecyclerView.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mAdapter = new CardViewAdapter(getDataSet());
-        mRecyclerView.setAdapter(mAdapter);
+        /**Internet availability implementation*/
+       /* if (connected){
+            accessWebService();
+            swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                @Override
+                public void onRefresh() {
+                    accessWebService();
+                    swipeRefreshLayout.setRefreshing(false);
+                }
+            });
+        }*/
+        /*else {
+            com.nispok.snackbar.Snackbar.with(Intro.this).text("No Internet Connection").actionLabel("Retry").actionListener(new ActionClickListener() {
+                @Override
+                public void onActionClicked(com.nispok.snackbar.Snackbar snackbar) {
+                    accessWebService();
+                    snackbar.dismiss();
+                }
+            }).show(Main2Activity.this);
+        }*/
+
+
 
         /**ViewPager Implementation*/
         final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
@@ -144,6 +168,32 @@ public class Main2Activity extends AppCompatActivity {
 
             }
         });
+/*
+        final Button activate = (Button) findViewById(R.id.activate);
+        final Button deactivate = (Button) findViewById(R.id.deactivate);
+        activate.setVisibility(View.GONE);
+        deactivate.setVisibility(View.VISIBLE);
+        activate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //when play is clicked show stop button and hide play button
+                activate.setVisibility(View.GONE);
+                deactivate.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        deactivate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //when play is clicked show stop button and hide play button
+                activate.setVisibility(View.VISIBLE);
+                deactivate.setVisibility(View.GONE);
+
+            }
+        });
+        */
+
     }
 
     @Override
@@ -155,8 +205,8 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected (MenuItem item){
         switch (item.getItemId()){
-            case R.id.action_leave_host:
-                LeaveHost();
+            case R.id.action_settings:
+                Settings2();
                 return true;
             case R.id.action_graph:
                 graph();
@@ -164,26 +214,111 @@ public class Main2Activity extends AppCompatActivity {
             case R.id.action_changeColour:
                 ChangeColour();
                 return true;
-            case R.id.action_add:
-                add();
-                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
+    /**Class extending the list gathered from the internet
+     * into the google card list view
+     * Backend stuff to initialize the cardview list
+     * FOR BACKEND GUYS TO DO****
+     */
+   /* private class MyTask extends AsyncTask<String, Void, List>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(Main2Activity.this);
+            progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            progressDialog.setIndeterminate(true);
+            progressDialog.setMessage("Please Wait");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected List doInBackground(String... params) {
+            try {
+                url = new URL(params[0]);
+                urlConnection =(HttpURLConnection) url.openConnection();
+                urlConnection.connect();
+                urlConnection.setConnectTimeout(5000);
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                StringBuilder jsonResult = inputStreamToString(in);
+                customList = new ArrayList<>();
+                JSONObject jsonResponseObject = new JSONObject(jsonResult.toString());
+
+                //JSONArray jsonResponseArray = new JSONArray(jsonResult.toString());
+                // [  for JSONArray
+                // { for JSONObject
+
+                JSONObject jsonMasterNode = jsonResponseObject.optJSONObject("data");
+                jsonMasterNode.getString("album");
+                System.out.println("MASTER NODE " + jsonMasterNode);
+
+                JSONArray jsonMainNode = jsonMasterNode.getJSONArray("movies");
+                System.out.println("LENGTH " + jsonMainNode.length());
+                System.out.println(jsonMainNode);
+
+                for(int i = 0; i < jsonMainNode.length(); i++){
+                    JSONObject jsonChildNode = jsonMainNode.getJSONObject(i);
+                    title = jsonChildNode.optString("title");
+                    album = jsonChildNode.optString("album");
+                    image = jsonChildNode.optString("urlPoster");
+                    //customList.add(new Main2Activity(title, album, image));
+
+                }
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
+            return customList;
+        }
+
+        private StringBuilder inputStreamToString(InputStream is) {
+            String rLine = " ";
+            StringBuilder answer = new StringBuilder();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            try {
+                while ((rLine = rd.readLine()) != null) {
+                    answer.append(rLine);
+                }
+            } catch (Exception e) {
+                Main2Activity.this.finish();
+            }
+            return answer;
+        }
+
+        @Override
+        protected void onPostExecute(List list) {
+            ListDrawer(list);
+            progressDialog.dismiss();
+        }
+    }
+    public void ListDrawer(List<ServerComms> customList) {
+        adapter = new CardViewHelper(customList);
+        adapter.notifyDataSetChanged();
+        recyclerView.setAdapter(adapter);
+    }
+
+
+    private void accessWebService(){
+        task = new MyTask();
+        task.execute(new String[] {URL});
+    }*/
+
     public void add(View v) {
         Intent intent = new Intent(this,AddActivity.class);
         startActivity(intent);
-    }
-    public void add() {
-        Intent intent = new Intent(this,AddActivity.class);
-        startActivity(intent);
-    }
-    private void LeaveHost(){
 
-        //Intent i = new Intent(Main2Activity.this, SettingsActivity2.class);
-        //startActivity(i);
+    }
+    private void Settings(){
+        Intent i = new Intent(Main2Activity.this, SettingsActivity.class);
+        startActivity(i);
+    }
+    private void Settings2(){
+        Intent i = new Intent(Main2Activity.this, SettingsActivity2.class);
+        startActivity(i);
     }
 
     //Temp Colour Change Activity
@@ -208,36 +343,6 @@ public class Main2Activity extends AppCompatActivity {
         finish();
         Intent i = new Intent(Main2Activity.this, Main2Activity.class);
         startActivity(i);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        ((CardViewAdapter) mAdapter).setOnItemClickListener(new CardViewAdapter
-                .MyClickListener() {
-            @Override
-            public void onItemClick(int position, View v) {
-                Log.i(LOG_TAG, " Clicked on Item " + position);
-            }
-        });
-    }
-
-    private ArrayList<DataObject> getDataSet() {
-        ArrayList results = new ArrayList<DataObject>();
-        for (int index = 0; index < 20; index++) {
-            DataObject obj = new DataObject("Some Primary Text " + index,
-                    "Secondary " + index);
-            results.add(index, obj);
-        }
-        return results;
-    }
-
-    private void setupActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            // Show the Up button in the action bar.
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
     }
 
 
