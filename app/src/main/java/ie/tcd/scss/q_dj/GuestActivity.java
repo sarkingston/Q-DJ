b3602aadd354f88477f705748a5a67840d8bbc31
owner <suleaa@tcd.ie>
2016-02-25 20:46:25 +0000

    added cardview code

--- app/src/main/java/ie/tcd/scss/q_dj/GuestActivity.java
+++ app/src/main/java/ie/tcd/scss/q_dj/GuestActivity.java
@@ -2,14 +2,55 @@ package ie.tcd.scss.q_dj;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+
+import java.util.ArrayList;
+
 /**
  * Created by Sam on 24/2/16.
  */
 public class GuestActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private static String LOG_TAG = "HostActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guest);
+        mRecyclerView = (RecyclerView) findViewById(R.id.card_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new CardViewAdapter(getDataSet());
+        mRecyclerView.setAdapter(mAdapter);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ((CardViewAdapter) mAdapter).setOnItemClickListener(new CardViewAdapter
+                .MyClickListener() {
+            @Override
+            public void onItemClick(int position, View v) {
+                Log.i(LOG_TAG, " Clicked on Item " + position);
+            }
+        });
+    }
+
+    private ArrayList<DataObject> getDataSet() {
+        ArrayList results = new ArrayList<DataObject>();
+        for (int index = 0; index < 20; index++) {
+            DataObject obj = new DataObject("Some Primary Text " + index,
+                    "Secondary " + index);
+            results.add(index, obj);
+        }
+        return results;
     }
 }
