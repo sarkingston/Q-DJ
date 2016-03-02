a1e3a7d24d21b7b5772479d4c1c08e0e296b8064
sidgvpta <siddharth.gvpta@gmail.com>
2016-03-02 23:39:37 +0000

    Changed Player buttons to reflect play/paused states

--- app/src/main/java/ie/tcd/scss/q_dj/HostActivity.java
+++ app/src/main/java/ie/tcd/scss/q_dj/HostActivity.java
@@ -1,10 +1,12 @@
 package ie.tcd.scss.q_dj;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
 
 import org.json.JSONException;
 
@@ -18,10 +20,34 @@ public class HostActivity extends AppCompatActivity {
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
+    com.getbase.floatingactionbutton.FloatingActionButton play;
+    boolean playActive;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host);
+
+        play = (com.getbase.floatingactionbutton.FloatingActionButton) findViewById(R.id.play);
+
+        final int idPlay = R.mipmap.ic_play_arrow_white_24dp;
+        final int idPause = R.mipmap.ic_pause_white_24dp;
+        play.setIcon(idPlay);
+        playActive = true;
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(playActive) {
+                    play.setIcon(idPause);
+                    playActive = false;
+                }
+                else {
+                    play.setIcon(idPlay);
+                    playActive = true;
+                }
+            }
+        });
+
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerList);
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
